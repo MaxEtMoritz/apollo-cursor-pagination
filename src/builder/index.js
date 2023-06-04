58833ba294d8fe2fbbafab38fb326485ebb7087a
MaxEtMoritz <markus@memotech.franken.de>
2023-06-04 16:39:23 +0200

    fix for my usecase
    not good but working for me now...

--- src/builder/index.js
+++ src/builder/index.js
@@ -1,3 +1,5 @@
+
+
 // based on Relay's Connection spec at
 // https://facebook.github.io/relay/graphql/connections.htm#sec-Pagination-algorithm
 
@@ -8,29 +10,40 @@
  * @param {Object} operatorFunctions must contain removeNodesBeforeAndIncluding and removeNodesAfterAndIncluding functions.
  * @param {Object} orderArgs must contain orderColumn and ascOrDesc. Include only if the implementor requires these params.
  */
-const applyCursorsToNodes = (
-  allNodesAccessor,
-  { before, after }, {
-    removeNodesBeforeAndIncluding,
-    removeNodesAfterAndIncluding,
-  }, {
-    orderColumn, ascOrDesc, isAggregateFn, formatColumnFn,
-  },
-) => {
+const applyCursorsToNodes = (allNodesAccessor, {
+  before,
+  after,
+}, {
+  removeNodesBeforeAndIncluding,
+  removeNodesAfterAndIncluding,
+}, {
+  orderColumn,
+  ascOrDesc,
+  isAggregateFn,
+  formatColumnFn,
+}) => {
   let nodesAccessor = allNodesAccessor;
+
   if (after) {
     nodesAccessor = removeNodesBeforeAndIncluding(nodesAccessor, after, {
-      orderColumn, ascOrDesc, isAggregateFn, formatColumnFn,
+      orderColumn,
+      ascOrDesc,
+      isAggregateFn,
+      formatColumnFn,
     });
   }
+
   if (before) {
     nodesAccessor = removeNodesAfterAndIncluding(nodesAccessor, before, {
-      orderColumn, ascOrDesc, isAggregateFn, formatColumnFn,
+      orderColumn,
+      ascOrDesc,
+      isAggregateFn,
+      formatColumnFn,
     });
   }
+
   return nodesAccessor;
 };
-
 /**
  * Slices a node list according to `before`, `after`, `first` and `last` graphql query params.
  * @param {Object} allNodesAccessor an accessor to the nodes. Will depend on the concrete implementor.
@@ -38,61 +51,88 @@ const applyCursorsToNodes = (
  * @param {Object} graphqlParams must contain `first`, `last`, `before` and `after` query params.
  * @param {Object} orderArgs must contain orderColumn and ascOrDesc. Include only if the implementor requires these params.
  */
-const nodesToReturn = async (
-  allNodesAccessor,
-  {
+
+const nodesToReturn = async (allNodesAccessor, {
+  removeNodesBeforeAndIncluding,
+  removeNodesAfterAndIncluding,
+  hasLengthGreaterThan,
+  removeNodesFromEnd,
+  removeNodesFromBeginning,
+  orderNodesBy,
+}, {
+  before,
+  after,
+  first,
+  last,
+}, {
+  orderColumn,
+  ascOrDesc,
+  isAggregateFn,
+  formatColumnFn,
+}) => {
+  const orderedNodesAccessor = orderNodesBy(allNodesAccessor, {
+    orderColumn,
+    ascOrDesc,
+    isAggregateFn,
+    formatColumnFn,
+  });
+  const nodesAccessor = applyCursorsToNodes(orderedNodesAccessor, {
+    before,
+    after,
+  }, {
     removeNodesBeforeAndIncluding,
     removeNodesAfterAndIncluding,
-    hasLengthGreaterThan,
-    removeNodesFromEnd,
-    removeNodesFromBeginning,
-    orderNodesBy,
-  },
-  {
-    before, after, first, last,
   }, {
-    orderColumn, ascOrDesc, isAggregateFn, formatColumnFn,
-  },
-) => {
-  const orderedNodesAccessor = orderNodesBy(allNodesAccessor, {
-    orderColumn, ascOrDesc, isAggregateFn, formatColumnFn,
+    orderColumn,
+    ascOrDesc,
+    isAggregateFn,
+    formatColumnFn,
   });
-  const nodesAccessor = applyCursorsToNodes(
-    orderedNodesAccessor,
-    { before, after },
-    {
-      removeNodesBeforeAndIncluding,
-      removeNodesAfterAndIncluding,
-    }, {
-      orderColumn, ascOrDesc, isAggregateFn, formatColumnFn,
-    },
-  );
   let hasNextPage = !!before;
   let hasPreviousPage = !!after;
   let nodes = [];
+
   if (first) {
     if (first < 0) throw new Error('`first` argument must not be less than 0');
-    nodes = await removeNodesFromEnd(nodesAccessor, first + 1, { orderColumn, ascOrDesc });
+    nodes = await removeNodesFromEnd(nodesAccessor, first + 1, {
+      orderColumn,
+      ascOrDesc,
+    });
+
     if (nodes.length > first) {
       hasNextPage = true;
       nodes = nodes.slice(0, first);
     }
   }
+
   if (last) {
     if (last < 0) throw new Error('`last` argument must not be less than 0');
-    nodes = await removeNodesFromBeginning(nodesAccessor, last + 1, { orderColumn, ascOrDesc });
+    nodes = await removeNodesFromBeginning(nodesAccessor, last + 1, {
+      orderColumn,
+      ascOrDesc,
+    });
+
     if (nodes.length > last) {
       hasPreviousPage = true;
       nodes = nodes.slice(1);
     }
   }
-  return { nodes, hasNextPage, hasPreviousPage };
-};
 
+  if (!first && !last) {
+    nodes = await nodesAccessor;
+  }
+
+  return {
+    nodes,
+    hasNextPage,
+    hasPreviousPage,
+  };
+};
 /**
  * Returns a function that must be called to generate a Relay's Connection based page.
  * @param {Object} operatorFunctions must contain `getNodesLength`, `removeNodesFromEnd`, `removeNodesFromBeginning`,`removeNodesBeforeAndIncluding` and `removeNodesAfterAndIncluding` functions.
  */
+
 const apolloCursorPaginationBuilder = ({
   removeNodesBeforeAndIncluding,
   removeNodesAfterAndIncluding,
@@ -102,19 +142,25 @@ const apolloCursorPaginationBuilder = ({
   removeNodesFromBeginning,
   convertNodesToEdges,
   orderNodesBy,
-}) => async (
-  allNodesAccessor,
-  {
-    before, after, first, last, orderBy = 'id', orderDirection = 'asc',
-  },
-  opts = {},
-) => {
+}) => async (allNodesAccessor, {
+  before,
+  after,
+  first,
+  last,
+  orderBy = 'id',
+  orderDirection = 'asc',
+}, opts = {}) => {
   const {
-    isAggregateFn, formatColumnFn, skipTotalCount = false, modifyEdgeFn,
+    isAggregateFn,
+    formatColumnFn,
+    skipTotalCount = false,
+    modifyEdgeFn,
   } = opts;
   let {
-    orderColumn, ascOrDesc,
+    orderColumn,
+    ascOrDesc,
   } = opts;
+
   if (orderColumn) {
     console.warn('"orderColumn" and "ascOrDesc" are being deprecated in favor of "orderBy" and "orderDirection" respectively');
   } else {
@@ -127,39 +173,50 @@ const apolloCursorPaginationBuilder = ({
     console.warn('This may cause issues with cursors being generated properly.');
   }
 
-  const { nodes, hasPreviousPage, hasNextPage } = await nodesToReturn(
-    allNodesAccessor,
-    {
-      removeNodesBeforeAndIncluding,
-      removeNodesAfterAndIncluding,
-      getNodesLength,
-      hasLengthGreaterThan,
-      removeNodesFromEnd,
-      removeNodesFromBeginning,
-      orderNodesBy,
-    }, {
-      before, after, first, last,
-    }, {
-      orderColumn, ascOrDesc, isAggregateFn, formatColumnFn,
-    },
-  );
-
-  const totalCount = !skipTotalCount && await getNodesLength(allNodesAccessor, {
+  const {
+    nodes,
+    hasPreviousPage,
+    hasNextPage,
+  } = await nodesToReturn(allNodesAccessor, {
+    removeNodesBeforeAndIncluding,
+    removeNodesAfterAndIncluding,
     getNodesLength,
+    hasLengthGreaterThan,
+    removeNodesFromEnd,
+    removeNodesFromBeginning,
+    orderNodesBy,
+  }, {
+    before,
+    after,
+    first,
+    last,
+  }, {
+    orderColumn,
+    ascOrDesc,
+    isAggregateFn,
+    formatColumnFn,
+  });
+  const totalCount = !skipTotalCount && (await getNodesLength(allNodesAccessor, {
+    getNodesLength,
+  }));
+  let edges = convertNodesToEdges(nodes, {
+    before,
+    after,
+    first,
+    last,
+  }, {
+    orderColumn,
+    ascOrDesc,
+    isAggregateFn,
+    formatColumnFn,
   });
 
-  let edges = convertNodesToEdges(nodes, {
-    before, after, first, last,
-  }, {
-    orderColumn, ascOrDesc, isAggregateFn, formatColumnFn,
-  });
   if (modifyEdgeFn) {
     edges = edges.map(edge => modifyEdgeFn(edge));
   }
 
   const startCursor = edges[0] && edges[0].cursor;
   const endCursor = edges[edges.length - 1] && edges[edges.length - 1].cursor;
-
   return {
     pageInfo: {
       hasPreviousPage,
